2c95e3ee9452c4cb19451dd45ee2a02b2fc51a55
juliatdm7 <juliatdm7@gmail.com>
2024-10-11 14:37:20 +0100

    Parameterising our Matrix Population Model (MPM): stage-specific survival rate and per capita reproduction.

--- code/PopDynamics_Practical2.R
+++ code/PopDynamics_Practical2.R
@@ -16,3 +16,45 @@ library(ggplot2)
 library(popbio)
 
 
+#1. Parameterising your Matrix Population Model (MPM)
+
+##a) Stage-specific survival
+##Survival rates where estimated using the data from the previous Practical using a Cormack–Jolly–Seber model in package "marked"
+##Time-varying detection probabilities were used when estimating the stage-specific survival rates, which were as follows:
+  S_j <- 0.463 #Juvenile survival from t --> t+1 (95% CI 0.404–0.524)
+  S_y <- 0.510 #Yearling survival from t --> t+1 (95% CI 0.445–0.574)
+  S_a <- 0.559 #Adult survival from t --> t+1 (95% CI 0.499–0.618)
+  
+  #First, let's enter the values into a dataframe 
+  survival <- data.frame(Stage=factor(c('Juvenile','Yearling','Adult'), levels=c('Juvenile','Yearling','Adult')), Estimate=c(S_j, S_y, S_a), lcl=c(0.404, 0.445, 0.499), ucl=c(0.524, 0.574, 0.618)) #We also include the lower and higher intervals of our Confidence Intervals
+  
+  #Then, let's plot by stage
+  ggplot(survival, aes(Stage, Estimate, ymin=lcl, ymax=ucl)) + 
+    geom_errorbar(width=0.2) + geom_point() + ylim(0,1)
+  
+  #QUESTION: Which stage has the lowest survival rate? Is this what you would expect?
+  #Juveniles have the lowest survival rate. Juveniles are very vulnerable to predation, nest abandonement, environmental adversities... and so it's expectable.
+  
+##b) Per capita reproduction
+##As we're only going to consider the female segment of our population, we need to estimate only the number of female offspring produced by each female between censuses.
+##This information can be gathered from the nest visitation data for Gjeroy for 1998-2012:
+  nestdata <- read.table("data/gjeroynest.txt", header = TRUE, sep = '\t') 
+  head(nestdata) #clutchno indicates whether it was the first, second or third etc. clutch laid in that nest in that breeding season. hatchingsuc indicates whether any live chicks were found for that clutch (yes = 1; no = 0). chickno indicates the number of chicks counted on the final visit prior to fledging.
+  
+  #Let's now estimate per capita reproduction
+  #We can calculate the average Hatching success as well as well as the average Fledging nr
+  HatchingSuc <- mean(nestdata$hatchingsuc)
+  FledglingNo <- mean(nestdata$chickno[nestdata$hatchingsuc == 1])
+  
+  #For the number of clutches, we first create a new dataframe called "nests" which has one row for each unique nest. 
+  #We add a column called numberofclutches which takes the maximum value of clutchno for each unique value of nestid. We then take the mean of these values to be the average number of clutches.
+  nests <- data.frame(nestid = sort(unique(nestdata$nestid)), numberofclutches=tapply(nestdata$clutchno, nestdata$nestid, max))  
+  ClutchNo <- mean(nests$numberofclutches)
+  
+  #We can use these as estimates of Clutch Number, Hatching Success and Fledgling Number to calculate per capita reproduction.
+  ##(Note! The mean number of chicks prior to fledging is an upwardly biased estimate of the number of fledglings, since not all will likely fledge the nest successfully).
+  
+  R <- (ClutchNo * HatchingSuc * FledglingNo) / 2 #We multiply the average number of clutches by the probability of hatching success by the average number of fledlings (when hatching is successful). We then divide by two as we're focusing only on the female section of our population and that section is around half of our population
+  #Our estimated per capita reproduction rate is 1.590824
+  
+  
